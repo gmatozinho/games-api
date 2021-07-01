a2c54a354a3574d918fe91172fef1373af98ae9b
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2021-06-30 23:26:26 -0300

    (fix): create with relations

--- src/handlers/create-item.js
+++ src/handlers/create-item.js
@@ -1,12 +1,11 @@
 const { create } = require("../repo/games");
 
-const event = {
-  "httpMethod": "POST",
-  "body": "{\"name\":\"God of war\",\"released\":\"2018\",\"website\":\"www.godofwar.com\",\"description\":\"deus grego\",\"created_at\":\"2021-06-22T01:50:24.327Z\",\"updated_at\":null,\"platforms\":[\"4cefcd09-558c-40ff-973f-ed81b40dc69d\"],\"tags\":[\"546cccae-831f-48d7-9234-3313d7db445a\"],\"stores\":[\"2a06b53b-0f1d-43e9-8b33-316720e32ff9\"]}"
-}
+/* const event = {
+  httpMethod: "POST",
+  body: '{"name":"God of war","released":"2018","website":"www.godofwar.com","description":"deus grego","created_at":"2021-06-22T01:50:24.327Z","updated_at":null,"platforms":["4cefcd09-558c-40ff-973f-ed81b40dc69d"],"tags":["546cccae-831f-48d7-9234-3313d7db445a"],"stores":["2a06b53b-0f1d-43e9-8b33-316720e32ff9"]}',
+}; */
 
-
-/* exports. */putItemHandler = async (/* event */) => {
+exports.createItemHandler = async (event) => {
   if (event.httpMethod !== "POST") {
     throw new Error(
       `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`
@@ -30,10 +29,8 @@ const event = {
   } catch (error) {
     const response = {
       statusCode: 500,
-      body: JSON.stringify({message: error.message}),
+      body: JSON.stringify({ message: error.message }),
     };
     return response;
   }
 };
-
-putItemHandler()
--- src/repo/games.js
+++ src/repo/games.js
@@ -1,58 +1,159 @@
 const { knex } = require("../lib/config");
+var knexnest = require("knexnest");
 
+const buildGame = ({ name, released, website, description, created_at }) => {
+  return { name, released, website, description, created_at };
+};
 
 const create = async (game) => {
   try {
-    const game_inserted = (await knex("game").returning("*").insert(game))[0];
-    const game_tag_inserted = (await knex("game_tag").returning("*").insert(insert_games_relations(game_inserted.id, game.tags,"TAGS")))[0];
-    const game_store_inserted = (await knex("game_store").returning("*").insert(insert_games_relations(game_inserted.id, game.stores,"STORE")))[0];
-    const game_platform_inserted = (await knex("game_platform").returning("*").insert(insert_games_relations(game_inserted.id, game.platforms,"PLATFORM")))[0];
-    return game_inserted;
+    const gameInserted = (
+      await knex("game").returning("*").insert(buildGame(game))
+    )[0];
+    const gameTagsInserted = await insertTagsRelation(
+      gameInserted.id,
+      game.tag
+    );
+    const gameStoresInserted = await insertStoresRelation(
+      gameInserted.id,
+      game.stores
+    );
+    const gamePlatformsInserted = await insertPlatformsRelation(
+      gameInserted.id,
+      game.platforms
+    );
+    return gameInserted;
   } catch (error) {
     throw error;
   }
 };
 
 const list = async () => {
-  const select = await knex("game").select().options({ nestTables: true });
-  return select;
+  try {
+    const sql = knex("game")
+      .select(
+        "game.id    AS _id",
+        "game.name AS _name",
+        "game.released AS _released",
+        "game.description AS _description",
+        "game.created_at AS _createdAt",
+        "game.website AS _website",
+        "game.updated_at AS _updatedAt",
+        "p.id    AS _platforms__id",
+        "p.name    AS _platforms__name",
+        "p.year_start    AS _platforms__yearStart",
+        "p.year_end    AS _platforms__yearEnd",
+        "s.id    AS _stores__id",
+        "s.name    AS _stores__name",
+        "s.website    AS _stores__website",
+        "t.id    AS _tags__id",
+        "t.name    AS _tags__name"
+      )
+      .innerJoin("game_platform AS gp", "gp.game_id", "game.id")
+      .innerJoin("game_store AS gs", "gs.game_id", "game.id")
+      .innerJoin("game_tag AS gt", "gt.game_id", "game.id")
+      .innerJoin("platform AS p", "p.id", "gp.platform_id")
+      .innerJoin("store AS s", "s.id", "gs.store_id")
+      .innerJoin("tag AS t", "t.id", "gt.tag_id");
+    const select = await knexnest(sql);
+    return select;
+  } catch (error) {
+    throw error;
+  }
 };
 
-const get_by_id = () => {};
+const getById = (id) => {
+  const sql = knex("game")
+    .select(
+      "game.id    AS _id",
+      "game.name AS _name",
+      "game.released AS _released",
+      "game.description AS _description",
+      "game.created_at AS _createdAt",
+      "game.website AS _website",
+      "game.updated_at AS _updatedAt",
+      "p.id    AS _platforms__id",
+      "p.name    AS _platforms__name",
+      "p.year_start    AS _platforms__yearStart",
+      "p.year_end    AS _platforms__yearEnd",
+      "s.id    AS _stores__id",
+      "s.name    AS _stores__name",
+      "s.website    AS _stores__website",
+      "t.id    AS _tags__id",
+      "t.name    AS _tags__name"
+    )
+    .innerJoin("game_platform AS gp", "gp.game_id", "game.id")
+    .innerJoin("game_store AS gs", "gs.game_id", "game.id")
+    .innerJoin("game_tag AS gt", "gt.game_id", "game.id")
+    .innerJoin("platform AS p", "p.id", "gp.platform_id")
+    .innerJoin("store AS s", "s.id", "gs.store_id")
+    .innerJoin("tag AS t", "t.id", "gt.tag_id")
+    .where("game.id", id);
+  const select = await knexnest(sql);
+  return select;
+};
 
 const update = () => {};
 
 const remove = () => {};
 
+const insertTagsRelation = (gameId, tags) => {
+  const gameTagInserted = (
+    await knex("game_tag")
+      .returning("*")
+      .insert(insertGamesRelations(gameId, tags, "TAG"))
+  )[0];
 
-const insert_games_relations = (game_id, array, relation ) =>{
-  const fields_to_insert = array.map((id)=>{
+  return gameTagInserted;
+};
+
+const insertStoresRelation = (gameId, stores) => {
+  const gameStoreInserted = (
+    await knex("game_store")
+      .returning("*")
+      .insert(insertGamesRelations(gameId, stores, "STORE"))
+  )[0];
+
+  return gameStoreInserted;
+};
+
+const insertPlatformsRelation = (gameId, platforms) => {
+  const gamePlatformInserted = (
+    await knex("game_platform")
+      .returning("*")
+      .insert(insertGamesRelations(gameId, platforms, "PLATFORM"))
+  )[0];
+
+  return gamePlatformInserted;
+};
+
+const insertGamesRelations = (gameId, array, relation) => {
+  const fieldsToInsert = array.map((id) => {
     return {
-      game_id,
-      [return_relation_field_name(relation)]: id
-    }
-  })
+      game_id: gameId,
+      [returnRelationFieldName(relation)]: id,
+    };
+  });
 
-  return fields_to_insert
-}
+  return fieldsToInsert;
+};
 
-
-const return_relation_field_name = (relation) =>{
+const returnRelationFieldName = (relation) => {
   switch (relation) {
     case "PLATFORM":
-      return "platform_id";      
+      return "platform_id";
     case "STORE":
-      return "store_id";      
+      return "store_id";
     case "TAG":
       return "tag_id";
-    default: 
-      break    
+    default:
+      break;
   }
-}
+};
 
 module.exports = {
   list,
-  get_by_id,
+  getById,
   create,
   update,
   remove,
