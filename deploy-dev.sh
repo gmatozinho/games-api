914b9d46f5fe015efcfc12f1eb03e6be15b7f20d
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2021-08-01 22:39:08 -0300

    (refactor): change bucket name

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-aws-vault exec study --no-session -- sam package --s3-bucket games-api-pipeline --template-file pipeline.yaml
-aws-vault exec study --no-session -- sam deploy --s3-bucket games-api-pipeline --stack-name games-api-pipeline \
+aws-vault exec study --no-session -- sam package --s3-bucket games-api --template-file pipeline.yaml
+aws-vault exec study --no-session -- sam deploy --s3-bucket games-api --stack-name games-api \
   --template-file pipeline.yaml --capabilities CAPABILITY_IAM 
